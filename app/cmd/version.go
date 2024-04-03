4262679f2a3423eb92e1c84eac0bff1b3b8d58d9
Jeremy Lewi <jeremy@lewi.us>
2024-04-03 11:31:45 -0700

    Scaffold the main application
    * Create the scaffolding for the main application.
    * This will handle the backend as well as serving the frontend.

---
+++ app/cmd/version.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/go-logr/zapr"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+package cmd
+
+import (
+"fmt"
+"io"
+
+"github.com/go-logr/zapr"
+"go.uber.org/zap"
+
+"github.com/spf13/cobra"
+)
+
+// These constants will be set by ldflags.
+// They can be set by goreleaser
+// https://goreleaser.com/cookbooks/using-main.version/?h=using+main.version
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
+func NewVersionCmd(name string, w io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "version",
+		Short:   "Return version",
+		Example: fmt.Sprintf("%s  version", name),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Fprintf(w, "%s %s, commit %s, built at %s by %s\n", name, version, commit, date, builtBy)
+		},
+	}
+	return cmd
+}
+
+func logVersion() {
+	log := zapr.NewLogger(zap.L())
+	log.Info("binary version", "version", version, "commit", commit, "date", date, "builtBy", builtBy)
+}
+
