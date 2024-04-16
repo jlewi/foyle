a21fe73143b0e15f877472056dc53838e2e91ca3
Jeremy Lewi <jeremy@lewi.us>
2024-04-16 12:38:02 -0700

    Add a test to demonstrate successful decoding.

--- app/pkg/logs/logs_test.go
+++ app/pkg/logs/logs_test.go
@@ -1,15 +1,22 @@
 package logs
 
 import (
+	"encoding/json"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/jlewi/foyle/app/pkg/testutil"
 	"github.com/jlewi/foyle/protos/go/foyle/v1alpha1"
 	"go.uber.org/zap"
+	"google.golang.org/protobuf/encoding/protojson"
 	"os"
 	"testing"
 )
 
+// This is a test to demonstrate how to log a protobuf message using zap and the generated protobuf code.
+// https://stackoverflow.com/questions/68411821/correctly-log-protobuf-messages-as-unescaped-json-with-zap-logger
 func Test_ZapPB(t *testing.T) {
-	c := zap.NewDevelopmentConfig()
-	zap.NewProductionConfig()
+	c := zap.NewProductionConfig()
+
 	oFile, err := os.CreateTemp("", "logs.json")
 	if err != nil {
 		t.Fatalf("Failed to create temp file: %v", err)
@@ -36,5 +43,33 @@ func Test_ZapPB(t *testing.T) {
 			},
 		},
 	}
-	log.Info("Test_ZapPB", ZapPB("req", request))
+	log.Info("Test_ZapPB", zap.Object("req", request))
+	if err := log.Sync(); err != nil {
+		t.Logf("Ignoring sync logger: %v", err)
+	}
+
+	// Make sure we can decode it
+	file, err := os.Open(outputName)
+	if err != nil {
+		t.Fatalf("Failed to open output file: %v", err)
+	}
+
+	type logLine struct {
+		RawProto *json.RawMessage `json:"req"`
+	}
+
+	l := &logLine{}
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(l); err != nil {
+		t.Fatalf("Failed to decode log entry: %v", err)
+	}
+
+	actual := &v1alpha1.ExecuteResponse{}
+	if err := protojson.Unmarshal(*l.RawProto, actual); err != nil {
+		t.Fatalf("Failed to decode block log: %v", err)
+	}
+
+	if d := cmp.Diff(request, actual, testutil.BlockComparer, cmpopts.IgnoreUnexported(v1alpha1.ExecuteResponse{})); d != "" {
+		t.Errorf("Unexpected diff: %v", d)
+	}
 }
