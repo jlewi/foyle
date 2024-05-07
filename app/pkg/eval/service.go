db4ccd2ad17adadcf16c6a2f1cbc82fc41e661f3
Jeremy Lewi <jeremy@lewi.us>
2024-05-06 17:33:01 -0700

    Implemented the EvalServiceList request.

--- app/pkg/eval/service.go
+++ app/pkg/eval/service.go
@@ -3,8 +3,12 @@ package eval
 import (
 	"connectrpc.com/connect"
 	"context"
+	"github.com/cockroachdb/pebble"
+	"github.com/jlewi/foyle/app/pkg/logs"
 	"github.com/jlewi/foyle/protos/go/foyle/v1alpha1"
-	"log"
+	"github.com/jlewi/monogo/helpers"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/proto"
 )
 
 // EvalServer is the server that implements the Eval service interface.
@@ -15,16 +19,52 @@ func (s *EvalServer) List(
 	ctx context.Context,
 	req *connect.Request[v1alpha1.EvalResultListRequest],
 ) (*connect.Response[v1alpha1.EvalResultListResponse], error) {
-	log.Println("Request headers: ", req.Header())
-	res := connect.NewResponse(&v1alpha1.EvalResultListResponse{
-		Items: []*v1alpha1.EvalResult{
-			{
-				Example: &v1alpha1.Example{
-					Id: "hello-world",
-				},
-			},
-		},
-	})
+	log := logs.FromContext(ctx)
+
+	if req.Msg.GetDatabase() == "" {
+		err := connect.NewError(connect.CodeInvalidArgument, errors.New("Request is missing database"))
+		log.Error(err, "Invalid EvalResultListRequest")
+		return nil, err
+	}
+
+	db, err := pebble.Open(req.Msg.GetDatabase(), &pebble.Options{})
+	if err != nil {
+		log.Error(err, "Failed to open database")
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+	defer helpers.DeferIgnoreError(db.Close)
+
+	iter, err := db.NewIterWithContext(ctx, nil)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+	defer iter.Close()
+
+	results := &v1alpha1.EvalResultListResponse{
+		Items: make([]*v1alpha1.EvalResult, 0, 100),
+	}
+
+	for iter.First(); iter.Valid(); iter.Next() {
+		key := iter.Key()
+		if key == nil {
+			break
+		}
+
+		value, err := iter.ValueAndErr()
+		if err != nil {
+			log.Error(err, "Failed to read value for key", "key", string(key))
+			continue
+		}
+
+		result := &v1alpha1.EvalResult{}
+		if err := proto.Unmarshal(value, result); err != nil {
+			log.Error(err, "Failed to unmarshal value for", "key", string(key))
+			continue
+		}
+		results.Items = append(results.Items, result)
+	}
+
+	res := connect.NewResponse(results)
 	res.Header().Set("Eval-Version", "v1alpha1")
 	return res, nil
 }
