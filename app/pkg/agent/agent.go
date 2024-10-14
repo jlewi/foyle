d868dc5229fa7a30fe467e51b98c503ba2f96647
Jeremy Lewi <jeremy@lewi.us>
2024-10-13 17:05:58 -0700

    Bump the maximum number of characters in the response because otherwise the output of previous cells can cause us to exceed the limits.

--- app/pkg/agent/agent.go
+++ app/pkg/agent/agent.go
@@ -43,7 +43,9 @@ const (
 	// We use the heuristic 1 token ~ 2 characters
 	// For details of how we came up with this see
 	// (https://platform.openai.com/docs/models/gpt-3-5-turbo)
-	MaxDocChars = 1110
+	// We bumped this up because we are using gpt4o-mini and want to include outputs of previous cells which can be quite
+	// long
+	MaxDocChars = 11100
 	temperature = 0.9
 )
 
